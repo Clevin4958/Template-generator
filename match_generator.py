819bb84d63b9b1be153e4c53aeca93ec26e9a3fd
Kevin Hu <hyxkv925@gmail.com>
2021-05-04 23:17:50 +1200

    Generate compscore file & export to csv feature

--- match_generator.py
+++ match_generator.py
@@ -1,4 +1,6 @@
 import numpy as np
+import csv
+from math import ceil
 def generate_RR(n):
     '''
     Generate round robin line-up for n teams in a round robin event
@@ -42,9 +44,67 @@ def generate_RR(n):
         matches = np.append(matches, [flatten_teams(teams)], axis=0)
         # put last team to first place
         teams = np.concatenate([[teams[-1]], teams[:-1]])
-        print(matches, teams)
         # print(flatten_teams(teams))
     return matches
 
+def gen_csv(matches, highest_set=2):
+    '''
+    generate array in the form ready for csv. It will include header and table number.
+
+    Parameter:
+    ----------
+    n: int
+        number of tables
+    matches: m * n array
+        m rows (matches) of n tables
+    highest_set: int
+        number of sets of boards used, unlimited if set to -1.
+    '''
+    m, n = matches.shape
+
+    # set up header
+    header = ['Round'] + ['NS', 'EW', 'Boards'] * n
+    table_row = ['Table->'] + [None] * (n * 3)
+    for i in range(n):
+        table_row[3 * i + 1] = i + 1
+
+    # transpose matches
+    matches = np.transpose(matches)
+
+    # prepare set number columns
+    bd_sets = []
+    highest_set = 2
+    m = 5
+    if highest_set == -1:
+        bd_sets = np.array(range(1, m+1))
+    else:
+        unit = range(1, highest_set + 1)
+        # repeat the unit set and take first m elem
+        bd_sets = np.array(list(unit) * ceil(m / highest_set))[:m]
+
+    # get center section of the output (product is 1d array)
+    result = np.array(range(1, m+1))
+    for i in range(n):
+        # concat result, NS, ES, boards
+        NS = np.empty(m)
+        NS.fill(i + 1)
+        result = np.concatenate((result, NS, matches[i], bd_sets))
+
+    # transpose back
+    result = result.astype('int32')
+    result = result.reshape((3*n+1, m))
+    result = np.transpose(result)
+
+    print([header, table_row], result)
+    # concat headers
+    result = np.concatenate([[header, table_row], result], axis=0)
+    return result.tolist()
+
+def export(content, file_name):
+    with open(file_name, 'w', newline='') as f:
+        write = csv.writer(f, lineterminator=',\r\n')
+        write.writerows(content)
+
 if __name__ == '__main__':
-    print(generate_RR(14))
+    data = gen_csv(generate_RR(6))
+    export(data, '6 team RR.csv')
